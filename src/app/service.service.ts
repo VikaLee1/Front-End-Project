ef4b95b37e6cfc46b618a5531150d1f3c55be0fd
Nikola Sijan <nikola.sijan992@gmail.com>
2022-10-19 10:38:11 +0200

    shop

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { IProducts } from './items/InterfaceItems';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
+  products:Array<IProducts>=[]
+
   constructor() { }
+
+
+  addToCart(product:IProducts){
+    // this.items.push(dish)
+    if (this.products.find((val) => val.name == product.name)) {
+      
+      product.qtty++;
+    } else {
+      this.products.push(product);
+    }
+  }
+
+  showProduct(){
+    return this.products
+  }
+
 }
